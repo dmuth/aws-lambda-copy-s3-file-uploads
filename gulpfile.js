076a40bf9869be7c9536fceb4d3c2c99371dd7ae
Douglas Muth <dmuth@dmuth.org>
2016-05-08 23:39:20 -0400

    Added first draft of upload task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 
 
+var exec = require('child_process').exec;
+
 var gulp = require("gulp");
 var zip = require("gulp-zip");
 
 
+//
+// Zip up our code and modules.
+//
 gulp.task("zip", function() {
 	return(
 		gulp.src(["copyFileFromS3.js", "node_modules/**/*"])
@@ -12,5 +17,33 @@ gulp.task("zip", function() {
 		);
 
 });
+
+
+//
+// Upload our zipfile
+//
+gulp.task("upload", function(cb) {
+
+	var cmd = "aws lambda create-function "
+		+ "--region us-east-1 "
+		+ "--function-name copyFileFromS3 "
+		+ "--zip-file fileb://dist/copyFileFromS3.zip "
+		+ "--role arn:aws:iam::287061943401:role/lamba-execute "
+		+ "--handler copyFileFromS3.handler "
+		+ "--runtime nodejs4.3 "
+		+ "--profile lambda-test "
+		+ "--timeout 10 "
+		+ "--memory-size 1024";
+	//console.log("CMD", cmd); // Debugging
+
+	exec(cmd, function (err, stdout, stderr) {
+		console.log("STDOUT", stdout);
+		console.log("STDERR", stderr);
+		cb(err);
+	});
+
+
+});
+
 
 
